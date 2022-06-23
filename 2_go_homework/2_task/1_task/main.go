bf3ec8f8776aa96990c926636aba13f3a8cdac40
andrea esposito <shlema.teach@gmail.com>
2022-06-23 12:38:37 +0300

    add task2_1

---
+++ 2_go_homework/2_task/1_task/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	// var result []int
+	keys := make(map[int]bool)
+	newArr := []int{}
+	for _, entry := range arr {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			newArr = append(newArr, entry)
+		}
+	}
+	fmt.Println(keys)
+	fmt.Println(arr)
+	fmt.Println(newArr)
+
+}
